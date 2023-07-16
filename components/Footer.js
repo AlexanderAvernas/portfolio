4ae58a5937120a01c0c782f8d3942dc16b85e757
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2023-07-16 18:31:46 +0200

    Added footer

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='FooterContainer'>
+        <h4 className='ContactHeader'>Contact</h4>
+        <div className='ContactContainer'>
+            <p>Mobile: 07707207273</p>
+            <p>Email: Alexander.aver@hotmail.se</p>
+            <p>LinkedIn: sjksjdjdkjd</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- components/Prodjects.js
+++ components/Prodjects.js
@@ -22,13 +22,25 @@ const ProjectContainer = styled.div`
 display: flex;
 flex-direction: column;
 padding: 2rem 2rem ;
+@media (min-width: 769px) {
+    flex-direction: row;
+    justify-content: center;
+}
 `
 
+
 const ProjectTextContainer = styled.div`
+@media (min-width: 769px) {
+    width: 30rem;
+    padding-top: 2.5rem;
+}
 
 `
 
 const ProjectImageContainer = styled.div `
+@media (min-width: 769px) {
+    margin-left: 1rem ;
+}
 
 `
 
