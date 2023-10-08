480238a9321f4e297da43629bc93fb62d414aee2
Alexander-Avernas <AlexanderAvernas@users.noreply.github.com>
2023-10-08 09:25:28 +0200

    Moved styledcomponents to global CSS

--- components/Hero.js
+++ components/Hero.js
@@ -9,28 +9,28 @@ function Hero() {
         threshold: 0.2 // Percentage of the component visible to trigger the animation
     })
     return (
-        <BigContainer>
-            <HeroContainer>
+        <div className='BigImageContainer'>
+            <div className='HeroContainer'>
                 <motion.div
                    ref={ref}
                    initial={{ opacity: 0, x: -100 }} // Startposition till vänster
                    animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -100 }} // Ändra x för att flytta texten
                    transition={{ duration: 0.75 }}>
-                    <HeaderContainer>
-                        <Header>
+                    <div className='HeaderContainer'>
+                        <h1 className='HeroHeader'>
                             Frontend <br /> Developer
-                        </Header>
-                        <Secondheader>Alexander Avernäs</Secondheader>
+                        </h1>
+                        <h2 className='SeconHeroHeader'>Alexander Avernäs</h2>
                         {/* <Thirdheader>
                             Student at IT-högskolan <br />
                             Stockholm
                         </Thirdheader> */}
-                    </HeaderContainer>
+                    </div>
                 </motion.div>
-            </HeroContainer>
-            <HeroRightContainer>
-                <ImgContainer>
-                    <HeroImage
+            </div>
+            <div className='HeroRightContainer'>
+                <div className='ImgContainer'>
+                    <img className='HeroImage'
                         src="images/HeroImage.png"
                         alt="Profile imgage"
                     />
@@ -40,109 +40,109 @@ function Hero() {
                    initial={{ opacity: 0, x: 100 }} // Startposition till vänster
                    animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -100 }} // Ändra x för att flytta texten
                    transition={{ duration: 0.75 }}>
-                    <ProjectScrollContainer>
-                        <ScrollText>Scroll down for projects</ScrollText>
-                    </ProjectScrollContainer>
+                    <div className='HeroProjectScrollContainer'>
+                        <h5 className='HeroScrollText'>Scroll down for projects</h5>
+                    </div>
                     </motion.div>
-                </ImgContainer>
-            </HeroRightContainer>
-        </BigContainer>
+                </div>
+            </div>
+        </div>
     )
 }
 
 export default Hero
 
-const BigContainer = styled.div`
-    @media (min-width: 769px) {
-        display: flex;
-        flex-direction: row;
-    }
-`
+// const BigContainer = styled.div`
+//     @media (min-width: 769px) {
+//         display: flex;
+//         flex-direction: row;
+//     }
+// `
 
-const HeroContainer = styled.div`
-    background-color: #2C3E50;
-    height: 55vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+// const HeroContainer = styled.div`
+//     background-color: #2C3E50;
+//     height: 55vh;
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
 
-    @media screen and (min-width: 769px) {
-        justify-content: left;
-        padding: 10rem;
-        height: 100vh;
-        width: 60vw;
-    }
-`
-const Header = styled.h1`
-    color: #FF6B6B;
-    line-height: 1;
-    font-weight: 700;
-    font-size: 3.5rem;
-    margin-bottom: 0;
-    overflow: hidden; // Dölj texten som inte ryms i elementet
-    white-space: nowrap; // Förhindra att texten bryts över flera rader
+//     @media screen and (min-width: 769px) {
+//         justify-content: left;
+//         padding: 10rem;
+//         height: 100vh;
+//         width: 60vw;
+//     }
+// `
+// const Header = styled.h1`
+//     color: #FF6B6B;
+//     line-height: 1;
+//     font-weight: 700;
+//     font-size: 3.5rem;
+//     margin-bottom: 0;
+//     overflow: hidden; // Dölj texten som inte ryms i elementet
+//     white-space: nowrap; // Förhindra att texten bryts över flera rader
 
-    @media (min-width: 769px) {
-        font-size: 4rem;
-    }
-`
-const Secondheader = styled.h2`
-    color: white;
-    font-size: 2rem;
-    margin-bottom: 0;
-`
+//     @media (min-width: 769px) {
+//         font-size: 4rem;
+//     }
+// `
+// const Secondheader = styled.h2`
+//     color: white;
+//     font-size: 2rem;
+//     margin-bottom: 0;
+// `
 
 const Thirdheader = styled.h4`
     color: purple;
     font-size: 1.3rem;
 `
-const HeaderContainer = styled.div``
+// const HeaderContainer = styled.div``
 
-const HeroRightContainer = styled.div`
-    background-color: #333333;
-    height: 35vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+// const HeroRightContainer = styled.div`
+//     background-color: #333333;
+//     height: 35vh;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
 
-    @media (min-width: 769px) {
-        width: 40vw;
-        height: 100vh;
-    }
-`
+//     @media (min-width: 769px) {
+//         width: 40vw;
+//         height: 100vh;
+//     }
+// `
 
-const ImgContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin-top: -20vh;
+// const ImgContainer = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
+//     margin-top: -20vh;
 
-    @media (min-width: 769px) {
-        flex-direction: row;
-    }
-`
+//     @media (min-width: 769px) {
+//         flex-direction: row;
+//     }
+// `
 
-const HeroImage = styled.img`
-    width: 45vw;
-    position: relative;
-    /* top: -10vh; */
-    border: 2px solid white;
-    border-radius: 50%;
+// const HeroImage = styled.img`
+//     width: 45vw;
+//     position: relative;
+//     /* top: -10vh; */
+//     border: 2px solid white;
+//     border-radius: 50%;
 
-    @media (min-width: 769px) {
-        width: 12rem;
-        right: 10.2vw;
-        top: -1rem;
-    }
+//     @media (min-width: 769px) {
+//         width: 12rem;
+//         right: 10.2vw;
+//         top: -1rem;
+//     }
 
-    @media (min-width: 1550px) {
-        width: 15rem;
-        right: 11.5vw;
-        top: -1rem;
-    }
-`
+//     @media (min-width: 1550px) {
+//         width: 15rem;
+//         right: 11.5vw;
+//         top: -1rem;
+//     }
+// `
 
 const ImgHeader = styled.h2`
     margin-top: 1rem;
@@ -154,30 +154,30 @@ const ImgHeader = styled.h2`
     }
 `
 
-const ProjectScrollContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 2rem;
+// const ProjectScrollContainer = styled.div`
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     margin-top: 2rem;
 
-    @media (min-width: 769px) {
-        margin-left: 5rem;
-        margin-right: 0rem;
-        margin-top: 34rem;
-    }
-`
+//     @media (min-width: 769px) {
+//         margin-left: 5rem;
+//         margin-right: 0rem;
+//         margin-top: 34rem;
+//     }
+// `
 
-const ScrollText = styled.h5`
-    padding: 0.5rem;
-    width: 40vw;
-    margin-right: 0.5rem;
-    margin-top: 1rem;
-    color: white;
-    font-weight: 500;
-    text-align: center;
+// const ScrollText = styled.h5`
+//     padding: 0.5rem;
+//     width: 40vw;
+//     margin-right: 0.5rem;
+//     margin-top: 1rem;
+//     color: white;
+//     font-weight: 500;
+//     text-align: center;
 
-    @media (min-width: 769px) {
-        width: 12vw;
-        padding: 0.75rem 0rem;
-    }
-`
+//     @media (min-width: 769px) {
+//         width: 12vw;
+//         padding: 0.75rem 0rem;
+//     }
+// `
--- components/Prodjects.js
+++ components/Prodjects.js
@@ -16,88 +16,88 @@ function Prodjects(props) {
             animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : 100 }} // Change opacity and x position based on inView
             transition={{ duration: 0.75 }}
         >
-            <ProjectContainer>
-                <ProjectTextContainer>
-                    <ProdjectInfo>{props.info}</ProdjectInfo>
-                    <SkillContainer>
+            <div className='ProjectComponentContainer'>
+                <div className='ProjectTextContainer'>
+                    <h4 className='ProdjectInfo'>{props.info}</h4>
+                    <div className='SkillContainer'>
                     {props.skills.map((skill, index) => (
-                            <Skill key={index}>{skill}</Skill>
+                            <p className='ProjectSkill' key={index}>{skill}</p>
                         ))}
-                    </SkillContainer>
-                    <ProjectLink>{props.link}</ProjectLink>
-                </ProjectTextContainer>
-                <ProjectImageContainer>
+                    </div>
+                    <p className='ProjectLink'>{props.link}</p>
+                </div>
+                <div className='ProjectImageContainer'>
                     <a
                         href={props.projectLink}
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <ImageProdject
+                        <img className='ImageProdject'
                             href={props.projectLink}
                             src={props.image}
                             alt="JavaScript project"
                         />
                     </a>
-                </ProjectImageContainer>
-            </ProjectContainer>
+                </div>
+            </div>
         </motion.div>
     )
 }
 
 export default Prodjects
 
-const ProjectContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 2rem 2rem;
-    @media (min-width: 769px) {
-        flex-direction: row;
-        justify-content: center;
-    }
-`
+// const ProjectContainer = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     padding: 2rem 2rem;
+//     @media (min-width: 769px) {
+//         flex-direction: row;
+//         justify-content: center;
+//     }
+// `
 
-const ProjectTextContainer = styled.div`
-    @media (min-width: 769px) {
-        width: 27rem;
-        padding-top: 4rem;
-    }
-`
+// const ProjectTextContainer = styled.div`
+//     @media (min-width: 769px) {
+//         width: 27rem;
+//         padding-top: 4rem;
+//     }
+// `
 
-const ProjectImageContainer = styled.div`
-    @media (min-width: 769px) {
-        margin-left: 1rem;
-    }
-`
+// const ProjectImageContainer = styled.div`
+//     @media (min-width: 769px) {
+//         margin-left: 1rem;
+//     }
+// `
 
-const ProdjectInfo = styled.h4``
+// const ProdjectInfo = styled.h4``
 
-const SkillContainer = styled.div `
-display: flex;
-margin-top: 0.8rem;
-`
+// const SkillContainer = styled.div `
+// display: flex;
+// margin-top: 0.8rem;
+// `
 
-const Skill = styled.p`
-border: 1px solid black;
-padding: 0.3rem 0.5rem;
-border-radius: 1rem;
-font-size: small;
-margin-right: 0.5rem;
-background-color: grey;
-color: white;
-`
+// const Skill = styled.p`
+// border: 1px solid black;
+// padding: 0.3rem 0.5rem;
+// border-radius: 1rem;
+// font-size: small;
+// margin-right: 0.5rem;
+// background-color: grey;
+// color: white;
+// `
 
-const ProjectLink = styled.p``
+// const ProjectLink = styled.p``
 
-const ImageProdject = styled.img`
-    width: 15rem;
-    margin-top: 0.5rem;
-    border: 0px solid black;
-    transition: transform 0.3s ease;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.9);
+// const ImageProdject = styled.img`
+//     width: 15rem;
+//     margin-top: 0.5rem;
+//     border: 0px solid black;
+//     transition: transform 0.3s ease;
+//     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.9);
 
-    /* Add hover effect */
-    &:hover {
-        transform: scale(1.05); /* Makes the image slightly bigger */
-         /* Adds a shadow on hover */
-    }
-`
+//     /* Add hover effect */
+//     &:hover {
+//         transform: scale(1.05); /* Makes the image slightly bigger */
+//          /* Adds a shadow on hover */
+//     }
+// `
